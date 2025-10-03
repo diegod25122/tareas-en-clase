3b3f5647e97df3d129f66449f1d471d9cf0d9200
POO <diego.camacho@epn.edu.ec>
2025-10-03 12:50:25 -0500

    clase

---
+++ src/Loginnnn.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Loginnnn {
+    public static void main(String[] args) {
+        String usuario ="diegouu2512";
+        String contrasenia = "251220004";
+        String user,password;
+        Scanner entrada = new Scanner(System.in);
+        while (true){
+            System.out.println("Ingrese su usuario: ");
+            user = entrada.nextLine();
+            System.out.println("Ingrese un contraseña: ");
+            password = entrada.nextLine();
+            //System.out.println(usuario==(usuario)); //equal para cadenas, == para numeros
+            if(user.equals(usuario) && password.equals(contrasenia)){
+                System.out.println("Acceso concedido");
+                break;
+            }else{
+                System.out.println("Usuario no valido");
+            }
+        }
+        entrada.close();
+    }
+}
